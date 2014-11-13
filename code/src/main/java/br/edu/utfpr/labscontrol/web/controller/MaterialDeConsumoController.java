21cdb1dfd7b5a7e298fa670a92cc58e2b8992d4e
EdsonTofolo <edsontofolo@gmail.com>
2014-11-12 22:38:02 -0200

    Alterado DER, e criado botao no material de consumo. #20

--- code/src/main/java/br/edu/utfpr/labscontrol/web/controller/MaterialDeConsumoController.java
+++ code/src/main/java/br/edu/utfpr/labscontrol/web/controller/MaterialDeConsumoController.java
@@ -8,12 +8,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
+import javax.faces.event.ActionEvent;
+import javax.naming.Context;
+import java.math.BigDecimal;
+import java.math.MathContext;
+
 /**
  * Created by edson on 23/08/2014.
  */
 @Controller
 @Scope("view")
 public class MaterialDeConsumoController extends CrudController<MaterialDeConsumo, Integer> {
+
+    private BigDecimal quantidade;
 
     @Autowired
     private MaterialDeConsumoService materialDeConsumoService;
@@ -26,5 +33,18 @@ public class MaterialDeConsumoController extends CrudController<MaterialDeConsum
     @Override
     protected String getUrlFormPage() {
         return "/pages/cadastros/materialdeconsumo/materialDeConsumoForm.xhtml?faces-redirect=true";
+    }
+
+    public void addQtd(ActionEvent actionEvent) {
+
+        entity.setQtdAtual(entity.getQtdAtual().add(this.quantidade, MathContext.DECIMAL64));
+    }
+
+    public BigDecimal getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(BigDecimal quantidade) {
+        this.quantidade = quantidade;
     }
 }
